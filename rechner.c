fffafb04ed0e1699a149796babacb890b0976117
hansibansix <kringwald@runbox.com>
2019-10-28 16:58:30 +0100

    fertig

--- rechner.c
+++ rechner.c
@@ -1,9 +1,30 @@
-#include <stdio.h>
 #include <math.h>
-int main()
-{ int a,b,ergebnis;
+#include <stdio.h>
+int main() {
+  float a, b, ergebnis;
   char operand;
-  scanf("%d,%c,%d", &a,&operand,&b);
-  printf("%d %c %d", a,operand,b);
+  printf("Bitte Rechnung eingeben: \n");
+  scanf("%f", &a);
+  scanf("%c", &operand);
+  scanf("%f", &b);
+
+  switch (operand) {
+  case '+':
+    ergebnis = a + b;
+    break;
+  case '-':
+    ergebnis = a - b;
+    break;
+  case '*':
+    ergebnis = a * b;
+    break;
+  case '/':
+    ergebnis = a / b;
+    break;
+  }
+  printf("%.2f %c %.2f = %.2f \n", a, operand, b, ergebnis);
+  printf("\n");
+
+  main();
   return 0;
 }
