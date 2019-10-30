1cec31144f28b1847313e72f8b28dc85d064853e
hansibansix <kringwald@runbox.com>
2019-10-30 19:47:33 +0100

    hexcalc

---
+++ haha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int a;
+  printf("enter hex\n");
+  scanf("%x", &a);
+  printf("\n");
+  printf("%x\n", a);
+  printf("\n");
+  printf("%d\n", a);
+  printf("\n");
+  main();
+  return 0;
+}
